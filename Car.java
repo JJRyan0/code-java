5d9597a8286ae09ff012b7d17e9441239c36ea0a
John Ryan <johnjoseph.ryan0@gmail.com>
2021-06-25 21:59:05 +0100

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,199 @@
+
+//---Class to define car objects
+
+public class Car{
+	String color;
+	int mpg;
+	boolean isElectric;
+//Constructor 1
+	public Car(String carColor, int milesPerGallon){
+		color = carColor;
+		mpg = milesPerGallon;
+	}
+
+	public Car(boolean electricCar, int milesPerGallon)
+		isElectric = electricCar;
+		mpg = milesPerGallon;
+	}
+}
+
+
+//-------------------
+
+//Inhertitance
+//Super class:
+public class Manipulation(){	
+}
+//Sub class
+public class Addition extends Manipulation(){
+}
+
+//Array List
+// Fast iteration and Random access
+//its is an orederd colloection and not sorted
+public class Fruits{
+
+	Public static class void main(String[] args){
+		ArrayList <String> names = new ArrayList <String>();
+		names.add("apple");
+		names.add("cherry");
+		names.add("kiwi");
+		names.add("banana");
+		names.add("cherry");
+		System.out.println (names);
+	}
+}
+//Vector
+public class Fruits{
+	public static void main(String[] args){
+		Vector <String> names = new Vector <String>();
+		names.add("cherry");
+		names.add("apple");
+		names.add("kiwi");
+		names.add("banana");
+		names.add("cherry");
+		System.outprintln(names);
+	}
+}
+
+//Linked List
+public class Fruits{
+	public static void main(String[] args){
+		Linkedlist <String> names = new Linkedlist <String>();
+		names.add("banana");
+		names.add("cherry");
+		names.add("apple");
+		names.add("kiwi");
+		names.add("banana");
+		System.out.println(names);
+
+	}
+}
+
+//HashMap 
+public class Fruits{
+	public static void main(String[] args){
+		HashMap <String> names = new MashMap <String>();
+		names.add("key1","banana");
+		names.add("key2","orange");
+		names.add("key3","apple");
+		System.out.println(names);
+	}
+}
+
+
+public class Person{
+	public static void main(String[] args){
+		System.out.println("Iam a person");
+	}
+}
+
+
+public class Adiddition{
+	public static void main(String[] args){
+		addition add = new Addition();//object creation
+	}
+}
+
+
+
+
+public class Manipulation(){
+	public class Addition extends Manipulation(){
+
+	}
+}
+
+
+
+public class Fruits{
+	public static void main(String[] args){
+		HashMap <String> names = new HashMap <String>();
+		names.add("key1","banana");
+		names.add("Key2","orange");
+
+	}
+}
+
+
+public class Fruit{
+	public static void main(String[] args){
+		HashMap <String> names = new Hashmap <String>();
+		names.add() 
+	}
+}
+
+
+
+public class Fruits{
+	public static void main(String args){
+		HashMap <String> names = new Hashmap <String>();
+		names.add('Key1','John');
+		names.add('Key2','Ryan');
+		names.add('Key3','Tom');
+	}
+}
+
+
+public class Fruits{
+	public class static void(String args){
+		Hashmap <String> names = new Hashmap <String>();
+		names.add('Key1','john')
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
